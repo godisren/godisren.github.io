25158b18eeba3f4da612bf46a18794ab0abe7ea7
godisren <god_is_ren@yahoo.com.tw>
2016-02-29 21:15:34 +0800

    add slider effect

--- camera_store/js/all.js
+++ camera_store/js/all.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
+
+	initScrollTop();
+	initBannerSlider();
 	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 200) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
+	function initScrollTop(){
+		//Check to see if the window is top if not then display button
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 200) {
+				$('.scrollToTop').fadeIn();
+			} else {
+				$('.scrollToTop').fadeOut();
+			}
+		});
+		
+		//Click event to scroll to top
+		$('.scrollToTop').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+		});
+	}
+
+	function initBannerSlider(){
+		//banner swiper effect
+		var bannerSwiper = new Swiper ('.banner',{loop:true,autoplay:2000});	
+
+		
+		var prodListSwiper = new Swiper ('.prod_list',{
+		 	loop:true,
+		 	nextButton: '.left_arrow',
+    		prevButton: '.right_arrow',
+    		noSwiping:true,
+		});	
+
+		//prodListSwiper.detachEvents();
+	}
 	
 });
